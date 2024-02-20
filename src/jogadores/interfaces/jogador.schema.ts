d66808c16a31acd28ca4c4a242330dece22b3129
pedromourax <pedrolucas.moura@hotmail.com>
2024-02-20 15:24:54 -0300

    propriedade 'telefone' agora pode ser duplicado

--- src/jogadores/interfaces/jogador.schema.ts
+++ src/jogadores/interfaces/jogador.schema.ts
@@ -2,7 +2,7 @@ import * as mongoose from 'mongoose'
 
 export const JogadorSchema = new mongoose.Schema({
     email: { type: String, unique: true },
-    telefone: { type: String, unique: true },
+    telefone: String,
     nome: String,
     ranking: String,
     posicaoRanking: Number,
