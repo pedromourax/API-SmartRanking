ae8f37594bcc98266f156dbebd078fcfc2ae6d32
pedromourax <pedrolucas.moura@hotmail.com>
2024-02-20 15:22:52 -0300

    importacao do dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { JogadoresModule } from './jogadores/jogadores.module';
 import { MongooseModule } from '@nestjs/mongoose';
-
+import * as dotenv from 'dotenv'
+dotenv.config()
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.DB_URI),
