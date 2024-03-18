3e5f0ba4640465fc1095e64b6a90c57017305652
pedromourax <pedrolucas.moura@hotmail.com>
2024-03-18 00:14:10 -0300

    Injecoes de dependencias

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { JogadoresModule } from './jogadores/jogadores.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CategoriasModule } from './categorias/categorias.module';
 import * as dotenv from 'dotenv'
 dotenv.config()
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.DB_URI),
-    JogadoresModule],
+    JogadoresModule,
+    CategoriasModule],
   controllers: [],
   providers: [],
 })
---
+++ src/categorias/categorias.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CategoriasController } from './categorias.controller';
+import { CategoriasService } from './categorias.service';
+import { CategoriaSchema } from './interfaces/categoria.schema';
+import { JogadoresModule } from 'src/jogadores/jogadores.module';
+
+@Module({
+    imports: [JogadoresModule, MongooseModule.forFeature([{ name: 'Categoria', schema: CategoriaSchema }])],
+    controllers: [CategoriasController],
+    providers: [CategoriasService]
+})
+export class CategoriasModule { }
