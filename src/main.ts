31ef445fd2b78c98d8382bfbf40c4692941c5943
pedromourax <pedrolucas.moura@hotmail.com>
2024-03-18 00:14:48 -0300

    Criacao de um exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv'
+import { AllExceptionFilter } from './common/filters/http-exception.filter';
 
 
 async function bootstrap() {
   dotenv.config()
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new AllExceptionFilter())
+
   await app.listen(3000);
 }
 bootstrap();
