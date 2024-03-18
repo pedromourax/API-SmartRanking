35b0554e96582477ffba009ae9893765ef9e0d4b
pedromourax <pedrolucas.moura@hotmail.com>
2024-03-18 00:16:04 -0300

    validacao de parametros

--- src/common/pipes/validacao-parametros.pipe.ts
+++ src/common/pipes/validacao-parametros.pipe.ts
@@ -1,6 +1,6 @@
 import { ArgumentMetadata, BadRequestException, PipeTransform } from "@nestjs/common";
 
-export class JogadoresValidacaoParametrosPipe implements PipeTransform {
+export class ValidacaoParametrosPipe implements PipeTransform {
     transform(valor: any, metadata: ArgumentMetadata) {
         if (!valor) throw new BadRequestException(`O valor do ${metadata.data} deve ser preenchido!`)
 
