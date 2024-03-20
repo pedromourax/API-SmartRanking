208816e0b086597c93e68d04d4b6082eec2d9b17
pedromourax <pedrolucas.moura@hotmail.com>
2024-03-19 23:22:15 -0300

    Schema da entidade 'partida'

---
+++ src/desafios/interfaces/partida.schema.ts
@@ -0,0 +1,16 @@
+import { Schema } from "mongoose";
+
+export const PartidaSchema = new Schema({
+    categoria: String,
+    jogadores: [{
+        type: Schema.Types.ObjectId,
+        ref: "Jogador"
+    }],
+    def: {
+        type: Schema.Types.ObjectId,
+        ref: "Jogador"
+    },
+    resultado: [{
+        set: String
+    }]
+}, { timestamps: true, collection: 'partidas' })
